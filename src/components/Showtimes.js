462119385e38fc63ad70a5aee1e3e95e927c0af2
Pedro Henrique <phcatanduba@gmail.com>
2021-05-12 00:21:08 -0300

    showtimes page -> style + api request

--- src/components/Showtimes.js
+++ src/components/Showtimes.js
@@ -1,5 +1,39 @@
 import Header from './Header';
+import Instruction from './Instruction';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export default function Showtimes() {
-    return <Header />;
+    const { idFilme } = useParams();
+    const [calendary, setCalendary] = useState([]);
+    console.log(calendary);
+    useEffect(() => {
+        const promise = axios.get(
+            `https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/movies/${idFilme}/showtimes`
+        );
+        promise.then((response) => {
+            setCalendary(response.data.days);
+        });
+    }, []);
+    if (calendary.length === 0) {
+        return <main>Carregando...</main>;
+    }
+    return (
+        <main>
+            <Header />
+            <Instruction text="Selecione o horário" />
+            {calendary.map((i) => {
+                return (
+                    <div className="date">
+                        <div>
+                            {i.weekday} - {i.date}
+                        </div>
+                        <button>{calendary[0].showtimes[0].name}</button>
+                    </div>
+                );
+            })}
+        </main>
+    );
 }
